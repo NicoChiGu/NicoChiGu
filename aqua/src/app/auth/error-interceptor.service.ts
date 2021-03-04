e41f0c5b448b23b088ab99aef21ae84e0687a8ed
Nico_ChiGu <34607782+NicoChiGu@users.noreply.github.com>
2021-03-05 04:07:11 +0800

    Add files via upload

---
+++ aqua/src/app/auth/error-interceptor.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorInterceptorService implements HttpInterceptor {
+
+  constructor() {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      console.log(err);
+      const error = err.error ? err.error.message : err.status + err.statusText;
+      return throwError(error);
+    }));
+  }
+}
