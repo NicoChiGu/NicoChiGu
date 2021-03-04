ef20cb371827404eef1db39ac8e637b7846316a7
Nico_ChiGu <34607782+NicoChiGu@users.noreply.github.com>
2021-03-05 04:12:14 +0800

    Add files via upload

---
+++ aqua/src/app/database/database.module.ts
@@ -0,0 +1,133 @@
+import {NgModule} from '@angular/core';
+import {DBConfig, NgxIndexedDBModule} from 'ngx-indexed-db';
+
+export function migrationFactory() {
+  return {};
+}
+const dbConfig: DBConfig = {
+  name: 'Aqua',
+  version: 2,
+  objectStoresMeta: [
+    {
+      store: 'divaPv',
+      storeConfig: {keyPath: 'pvId', autoIncrement: false},
+      storeSchema: [
+        {name: 'bpm', keypath: 'bpm', options: {unique: false}},
+        {name: 'date', keypath: 'date', options: {unique: false}},
+        {name: 'songName', keypath: 'songName', options: {unique: false}},
+        {name: 'songNameEng', keypath: 'songNameEng', options: {unique: false}},
+        {name: 'songNameReading', keypath: 'songNameReading', options: {unique: false}},
+        {name: 'arranger', keypath: 'arranger', options: {unique: false}},
+        {name: 'illustrator', keypath: 'illustrator', options: {unique: false}},
+        {name: 'lyrics', keypath: 'lyrics', options: {unique: false}},
+        {name: 'music', keypath: 'music', options: {unique: false}},
+        {name: 'difficulty', keypath: 'difficulty', options: {unique: false}},
+        {name: 'performerNumber', keypath: 'performerNumber', options: {unique: false}}
+      ]
+    }, {
+      store: 'divaModule',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'price', keypath: 'price', options: {unique: false}}
+      ]
+    }, {
+      store: 'divaCustomize',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'price', keypath: 'price', options: {unique: false}}
+      ]
+    }, {
+      store: 'chuniMusic',
+      storeConfig: {keyPath: 'musicId', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'sotrName', keypath: 'sotrName', options: {unique: false}},
+        {name: 'copyright', keypath: 'copyright', options: {unique: false}},
+        {name: 'artistName', keypath: 'artistName', options: {unique: false}},
+        {name: 'genre', keypath: 'genre', options: {unique: false}},
+        {name: 'releaseVersion', keypath: 'releaseVersion', options: {unique: false}}
+      ]
+    }, {
+      store: 'ongekiCard',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'nickName', keypath: 'nickName', options: {unique: false}},
+        {name: 'attribute', keypath: 'attribute', options: {unique: false}},
+        {name: 'charaId', keypath: 'charaId', options: {unique: false}},
+        {name: 'school', keypath: 'school', options: {unique: false}},
+        {name: 'gakuen', keypath: 'gakuen', options: {unique: false}},
+        {name: 'rarity', keypath: 'rarity', options: {unique: false}},
+        {name: 'levelParam', keypath: 'levelParam', options: {unique: false}},
+        {name: 'skillId', keypath: 'skillId', options: {unique: false}},
+        {name: 'chouKaikaSkillId', keypath: 'chouKaikaSkillId', options: {unique: false}},
+        {name: 'cardNumber', keypath: 'cardNumber', options: {unique: false}},
+        {name: 'version', keypath: 'version', options: {unique: false}},
+      ]
+    }, {
+      store: 'ongekiCharacter',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'cv', keypath: 'cv', options: {unique: false}},
+        {name: 'modelId', keypath: 'modelId', options: {unique: false}}
+      ]
+    }, {
+      store: 'ongekiMusic',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'sortName', keypath: 'sortName', options: {unique: false}},
+        {name: 'artistName', keypath: 'artistName', options: {unique: false}},
+        {name: 'genre', keypath: 'genre', options: {unique: false}},
+        {name: 'bossCardId', keypath: 'bossCardId', options: {unique: false}},
+        {name: 'bossLevel', keypath: 'bossLevel', options: {unique: false}},
+        {name: 'level0', keypath: 'level0', options: {unique: false}},
+        {name: 'level1', keypath: 'level1', options: {unique: false}},
+        {name: 'level2', keypath: 'level2', options: {unique: false}},
+        {name: 'level3', keypath: 'level3', options: {unique: false}},
+        {name: 'level4', keypath: 'level4', options: {unique: false}}
+      ]
+    }, {
+      store: 'ongekiSkill',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'sortName', keypath: 'sortName', options: {unique: false}},
+        {name: 'category', keypath: 'category', options: {unique: false}},
+        {name: 'info', keypath: 'info', options: {unique: false}}
+      ]
+    }, {
+      store: 'chuniCharacter',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'releaseTag', keypath: 'releaseTag', options: {unique: false}},
+        {name: 'worksName', keypath: 'worksName', options: {unique: false}},
+        {name: 'illustratorName', keypath: 'illustratorName', options: {unique: false}},
+        {name: 'firstSkillId', keypath: 'firstSkillId', options: {unique: false}},
+        {name: 'skills', keypath: 'skills', options: {unique: false}},
+        {name: 'addImages', keypath: 'addImages', options: {unique: false}}
+      ]
+    }, {
+      store: 'chuniSkill',
+      storeConfig: {keyPath: 'id', autoIncrement: false},
+      storeSchema: [
+        {name: 'name', keypath: 'name', options: {unique: false}},
+        {name: 'category', keypath: 'category', options: {unique: false}}
+      ]
+    },
+  ],
+  migrationFactory
+};
+
+@NgModule({
+  declarations: [],
+  imports: [
+    NgxIndexedDBModule.forRoot(dbConfig)
+  ]
+})
+export class DatabaseModule {
+}
---
+++ aqua/src/app/database/preload.service.ts
@@ -0,0 +1,102 @@
+import {Injectable} from '@angular/core';
+import {NgxIndexedDBService} from 'ngx-indexed-db';
+import {ReplaySubject} from 'rxjs';
+import {ApiService} from '../api.service';
+import {DivaPv} from '../sega/diva/model/DivaPv';
+import {ChuniMusic} from '../sega/chunithm/amazon/model/ChuniMusic';
+import {DivaModule} from '../sega/diva/model/DivaModule';
+import {DivaCustomize} from '../sega/diva/model/DivaCustomize';
+import {OngekiCard} from '../sega/ongeki/model/OngekiCard';
+import {OngekiCharacter} from '../sega/ongeki/model/OngekiCharacter';
+import {OngekiMusic} from '../sega/ongeki/model/OngekiMusic';
+import {OngekiSkill} from '../sega/ongeki/model/OngekiSkill';
+import {ChuniCharacter} from '../sega/chunithm/amazon/model/ChuniCharacter';
+import {ChuniSkill} from '../sega/chunithm/amazon/model/ChuniSkill';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreloadService {
+
+  private divaPv = new ReplaySubject<string>();
+  divaPvState = this.divaPv.asObservable();
+  private divaModule = new ReplaySubject<string>();
+  divaModuleState = this.divaModule.asObservable();
+  private divaCustomize = new ReplaySubject<string>();
+  divaCustomizeState = this.divaCustomize.asObservable();
+
+  private chuniMusic = new ReplaySubject<string>();
+  chuniMusicState = this.chuniMusic.asObservable();
+
+  private ongekiCard = new ReplaySubject<string>();
+  ongekiCardState = this.ongekiCard.asObservable();
+  private ongekiCharacter = new ReplaySubject<string>();
+  ongekiCharacterState = this.ongekiCharacter.asObservable();
+  private ongekiMusic = new ReplaySubject<string>();
+  ongekiMusicState = this.ongekiMusic.asObservable();
+  private ongekiSkill = new ReplaySubject<string>();
+  ongekiSkillState = this.ongekiSkill.asObservable();
+
+  private chuniCharacter = new ReplaySubject<string>();
+  chuniCharacterState = this.chuniCharacter.asObservable();
+  private chuniSkill = new ReplaySubject<string>();
+  chuniSkillState = this.chuniSkill.asObservable();
+
+  constructor(
+    private dbService: NgxIndexedDBService,
+    private api: ApiService
+  ) {
+  }
+
+  load() {
+    this.loader<DivaPv>('divaPv', 'api/game/diva/data/musicList', this.divaPv);
+    this.loader<DivaModule>('divaModule', 'api/game/diva/data/moduleList', this.divaModule);
+    this.loader<DivaCustomize>('divaCustomize', 'api/game/diva/data/customizeList', this.divaCustomize);
+    this.loader<ChuniMusic>('chuniMusic', 'api/game/chuni/amazon/music', this.chuniMusic);
+    this.loader<OngekiCard>('ongekiCard', 'api/game/ongeki/data/cardList', this.ongekiCard);
+    this.loader<OngekiCharacter>('ongekiCharacter', 'api/game/ongeki/data/charaList', this.ongekiCharacter);
+    this.loader<OngekiMusic>('ongekiMusic', 'api/game/ongeki/data/musicList', this.ongekiMusic);
+    this.loader<OngekiSkill>('ongekiSkill', 'api/game/ongeki/data/skillList', this.ongekiSkill);
+    this.loader<ChuniCharacter>('chuniCharacter', 'api/game/chuni/amazon/data/character', this.chuniCharacter);
+    this.loader<ChuniSkill>('chuniSkill', 'api/game/chuni/amazon/data/skill', this.chuniSkill);
+  }
+
+  reload() {
+
+  }
+
+  loader<T>(storeName: string, url: string, status: ReplaySubject<string>) {
+    this.dbService.count(storeName).then(
+      pageCount => {
+        if (pageCount > 0) {
+          status.next('OK');
+        } else {
+          status.next('Downloading');
+          this.api.get(url).subscribe(
+            data => {
+              let errorFlag = false;
+              data.forEach(x => {
+                this.dbService.add<T>(storeName, x).then(
+                  () => '', error => {
+                    console.error(error);
+                    errorFlag = true;
+                  }
+                );
+              });
+              if (errorFlag) {
+                status.next('Error');
+              } else {
+                status.next('OK');
+              }
+              status.complete();
+            },
+            error => {
+              console.error(error);
+              status.next('Error');
+              status.complete();
+            }
+          );
+        }
+      });
+  }
+}
